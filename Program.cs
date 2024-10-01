82795ef03b5b7a62f29fa6276b964d72c26d311c
jaqubm <jaqobgm@gmail.com>
2024-10-01 22:13:06 +0200

    Fix for CORS

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     
     options.AddPolicy("ProdCors", policyBuilder =>
     {
-        policyBuilder.WithOrigins("https://mathai.jaqubm.dev/")
+        policyBuilder.WithOrigins("https://mathai.jaqubm.dev")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
