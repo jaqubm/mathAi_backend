ce9fabf0f27381c1fac4bbecfb201dbc48cc7fea
jaqubm <jaqobgm@gmail.com>
2024-11-06 18:56:39 +0100

    Simplifying backend

---
+++ Dtos/ClassListDto.cs
@@ -0,0 +1,8 @@
+namespace mathAi_backend.Dtos;
+
+public class ClassListDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string OwnerId { get; set; } = string.Empty;
+}
---
+++ Dtos/ExerciseSetListDto.cs
@@ -0,0 +1,10 @@
+namespace mathAi_backend.Dtos;
+
+public class ExerciseSetListDto
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string SchoolType { get; set; } = "";
+    public string Grade { get; set; } = "";
+    public string Subject { get; set; } = "";
+}
